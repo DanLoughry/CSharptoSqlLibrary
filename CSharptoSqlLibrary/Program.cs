9d2d9c1527d48330c33e574d74a8ecef5924d212
Daniel Loughry <loughry_d@yahoo.com>
2018-06-07 11:03:59 -0400

    initial commit

--- CSharptoSqlLibrary/Program.cs
+++ CSharptoSqlLibrary/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SqlLibrary;
 
-namespace CSharptoSqlLibrary
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace CSharptoSqlLibrary{
+	public class Program	{
+		static void Main(string[] args)		{
+
+			UsersController UserCtrl = new UsersController();
+
 		}
 	}
 }
---
+++ SqlLibrary/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLibrary  {
+	public class UsersController 	{
+
+		public IEnumerable<User> List()	{   //creating a method called list with is an enumerable of list user
+			return new List<User>();					// do not return null, instead return an empty list 
+		}
+		public User Get(int id) 	{		//these are stb methods that are defined but do not do anything alone
+			return null;
+		}
+		public bool Create(User user)	{
+			return false;
+		}
+		public bool Change(User user)		{
+			return false;
+		}
+		public bool Remove(User user)	{
+			return false;
+		}
+	}
+}
